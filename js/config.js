6aa64e46edebd752e3a093e1e30e1ca2f25c661e
jatolyan) <twistplay@mail.ru>
2020-05-20 22:53:22 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -50,7 +50,7 @@ var l_background = "backgrounds/videos/glimmering.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-	"backgrounds/images/Alice.jpg",
+	"backgrounds/images/alice.jpg",
 ];
 
 /*Random background images order?*/
